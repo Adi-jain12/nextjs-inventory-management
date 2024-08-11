b3e9cf0117f24130d6a4fe667a2c954bc8cb6127
Adi-jain12 <jaditya98@gmail.com>
2024-08-11 14:37:40 +0530

    add configuration files

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,8 +25,8 @@ app.use('/api/v1/products', productRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/expenses', expenseRoutes);
 
-const port = process.env.PORT || 3001;
+const port = Number(process.env.PORT) || 3001;
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
 	console.log(`Server running on port ${port}`);
 });
